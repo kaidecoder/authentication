2d837e0e55f5789bcd0faf4ccfe46ef5e15137f8
kaidecoder <131601358+kaidecoder@users.noreply.github.com>
2024-03-12 11:41:09 -0400

    add a schema and model

---
+++ server/controllers/workoutController.js
@@ -0,0 +1,11 @@
+import Workout from "../models/workout.mjs";
+
+//GET all workouts
+
+//GET a single workout
+
+//POST a new workout
+
+//DELETE a single workout
+
+//PATCH a single workout
--- server/routes/workouts.mjs
+++ server/routes/workouts.mjs
@@ -1,13 +1,22 @@
 import express from "express"
+import Workout from "../models/workout.mjs"
+
 const router = express.Router()
 
 router.get("/", (req, res) => {
     console.log(req.query.msg)
     res.json({msg: "Get all workouts"})
 })
-router.post("/:id", (req, res) => {
-    console.log(req.body)
-    res.json({msg: "Create a workout"})
+router.post("/:id", async (req, res) => {
+    const {title, load, reps} = req.body
+    try {
+        const workout = await Workout.create({
+            title, load, reps
+        })
+        res.status(201).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 })
 router.get("/:id", (req, res) => {
     const {id}= req.params
